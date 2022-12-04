b86fe58a269f6a94f10ee0c011de66d8c1b2ba1a
bbenja <bbenja098@gmail.com>
2022-12-04 10:00:44 +0100

    added second tab

--- lib/home.dart
+++ lib/home.dart
@@ -14,8 +14,21 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   List<ToDo> _found = [];
   final _toDoController = TextEditingController();
-
   static late SharedPreferences sharedPreferences;
+  static const List<Tab> tabs = <Tab>[
+    Tab(
+      child: const Text(
+        "Personal",
+        style: TextStyle(fontSize: 18, color: Colors.black),
+      ),
+    ),
+    Tab(
+      child: const Text(
+        "Work",
+        style: TextStyle(fontSize: 18, color: Colors.black),
+      ),
+    )
+  ];
 
   @override
   void initState() {
@@ -48,98 +61,144 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
         backgroundColor: const Color(0xFFEEEFF5),
-        appBar: _buildAppBar(),
-        body: Stack(
-          children: [
-            Container(
-                height: 520,
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-                child: Column(
-                  children: [
-                    searchBox(),
-                    Container(
-                      margin: const EdgeInsets.only(top: 15, bottom: 15),
-                      child: Center(
-                        child: const Text(
-                          "All ToDos",
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.w500),
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      child: Material(
-                        color: const Color(0xFFEEEFF5),
-                        child: ListView(
-                          children: [
-                            for (ToDo todoo in _found.reversed)
-                              ToDoItem(
-                                todo: todoo,
-                                onToDoChanged: _handleToDoChange,
-                                onDeleteItem: _deleteToDoItem,
-                              ),
-                          ],
-                        ),
-                      ),
-                    )
-                  ],
-                )),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Container(
-                      margin: const EdgeInsets.only(
-                          bottom: 20, right: 20, left: 20),
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 20, vertical: 5),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        boxShadow: const [
-                          BoxShadow(
-                            color: Colors.grey,
-                            offset: Offset(0.0, 0.0),
-                            blurRadius: 10.0,
-                            spreadRadius: 0.0,
-                          ),
-                        ],
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: TextField(
-                          controller: _toDoController,
-                          decoration: const InputDecoration(
-                              hintText: "Add new todo",
-                              border: InputBorder.none)),
-                    ),
+        appBar: AppBar(
+            backgroundColor: const Color(0xFFEEEFF5),
+            elevation: 0,
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Icon(
+                  Icons.menu,
+                  color: Color(0xFF3a3a3a),
+                  size: 30,
+                ),
+                SizedBox(
+                  height: 40,
+                  width: 40,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.asset("assets/image/46.jpg"),
                   ),
-                  Container(
-                      margin: const EdgeInsets.only(
-                        bottom: 20,
-                        right: 20,
-                      ),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          _addToDoItem(_toDoController.text);
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.blue,
-                          minimumSize: const Size(60, 60),
-                          elevation: 10,
-                        ),
-                        child: const Text(
-                          "+",
-                          style: TextStyle(fontSize: 40),
-                        ),
-                      ))
-                ],
+                )
+              ],
+            ),
+            bottom: PreferredSize(
+              preferredSize: _tabBar.preferredSize,
+              child: Material(
+                color: Colors.white,
+                child: _tabBar,
               ),
-            )
-          ],
-        ));
+            )),
+        body: Container(
+          child: TabBarView(
+            children: [
+              Container(child: buildPersonalColumn()),
+              Container(child: Text("Other")),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  TabBar get _tabBar => TabBar(
+        tabs: tabs,
+      );
+
+  Column buildWorkColumn() {
+    return Column(
+      children: [
+        Expanded(
+            child: Scaffold(
+          body: Expanded(
+            child: Material(
+              color: const Color(0xFFEEEFF5),
+              child: ListView(
+                children: [Text("list2")],
+              ),
+            ),
+          ),
+        )),
+      ],
+    );
+  }
+
+  Column buildPersonalColumn() {
+    return Column(
+      children: [
+        Expanded(
+          child: Material(
+            color: const Color(0xFFEEEFF5),
+            child: ListView(
+              padding: EdgeInsets.all(15),
+              children: [
+                for (ToDo todoo in _found.reversed)
+                  ToDoItem(
+                    todo: todoo,
+                    onToDoChanged: _handleToDoChange,
+                    onDeleteItem: _deleteToDoItem,
+                  ),
+              ],
+            ),
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Row(
+            children: [
+              Expanded(
+                child: Container(
+                  margin:
+                      const EdgeInsets.only(bottom: 20, right: 20, left: 20),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.grey,
+                        offset: Offset(0.0, 0.0),
+                        blurRadius: 10.0,
+                        spreadRadius: 0.0,
+                      ),
+                    ],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: TextField(
+                      controller: _toDoController,
+                      decoration: const InputDecoration(
+                          hintText: "Add new todo", border: InputBorder.none)),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(
+                  bottom: 20,
+                  right: 20,
+                ),
+                child: ElevatedButton(
+                  onPressed: () {
+                    _addToDoItem(_toDoController.text);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    minimumSize: const Size(60, 60),
+                    elevation: 10,
+                  ),
+                  child: const Text(
+                    "+",
+                    style: TextStyle(fontSize: 40),
+                  ),
+                ),
+              )
+            ],
+          ),
+        )
+      ],
+    );
   }
 
   void _handleToDoChange(ToDo todo) {
@@ -207,25 +266,26 @@ class _HomeState extends State<Home> {
 
   AppBar _buildAppBar() {
     return AppBar(
-        backgroundColor: const Color(0xFFEEEFF5),
-        elevation: 0,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            const Icon(
-              Icons.menu,
-              color: Color(0xFF3a3a3a),
-              size: 30,
+      backgroundColor: const Color(0xFFEEEFF5),
+      elevation: 0,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Icon(
+            Icons.menu,
+            color: Color(0xFF3a3a3a),
+            size: 30,
+          ),
+          SizedBox(
+            height: 40,
+            width: 40,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset("assets/image/46.jpg"),
             ),
-            SizedBox(
-              height: 40,
-              width: 40,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: Image.asset("assets/image/46.jpg"),
-              ),
-            )
-          ],
-        ));
+          )
+        ],
+      ),
+    );
   }
 }
